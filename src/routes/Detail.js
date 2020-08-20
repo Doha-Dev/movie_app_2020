be5a6769a20605eeacf1cebe7c1738ae074142b3
Doha <zgggzgg14@gmail.com>
2020-08-20 22:10:52 +0900

    Chapter Complete react app

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./Detail.css";
 
 class Detail extends React.Component {
   componentDidMount() {
@@ -11,7 +12,22 @@ class Detail extends React.Component {
   render() {
     const { location } = this.props;
     if (location.state) {
-      return <span>{location.state.title}</span>;
+      return (
+        <div className="detail__container">
+          <span>TITLE : {location.state.title}</span>
+          <span>YEAR : {location.state.year}</span>
+          <ul className="detail__genres">
+            {location.state.genres.map((genre, index) => {
+              return (
+                <li key={index} className="detail__genre">
+                  {genre}
+                </li>
+              );
+            })}
+          </ul>
+          <span>SUMMARY : {location.state.summary}</span>
+        </div>
+      );
     } else {
       return null;
     }
