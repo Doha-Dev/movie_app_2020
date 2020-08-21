1e7d0bb58eb05a6c3239f619ba53b477c6013599
Doha <zgggzgg14@gmail.com>
2020-08-21 14:21:09 +0900

    Updates

---
+++ precache-manifest.babcd1f6e38282af0094ef2f020a5fb7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20376898cecb5e6d974ff29ffce86edb",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "d5d24703804c135d2511",
+    "url": "/movie_app_2020/static/css/main.d11e0b9e.chunk.css"
+  },
+  {
+    "revision": "da91fca76777d8e65fc5",
+    "url": "/movie_app_2020/static/js/2.e2494b61.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movie_app_2020/static/js/2.e2494b61.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5d24703804c135d2511",
+    "url": "/movie_app_2020/static/js/main.5ae929b9.chunk.js"
+  },
+  {
+    "revision": "6636fcdddd453d60abc3",
+    "url": "/movie_app_2020/static/js/runtime-main.9a083715.js"
+  }
+]);
